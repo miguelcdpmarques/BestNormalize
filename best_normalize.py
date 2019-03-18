35241df2159b89420881b130b8d42f8691006933
miguelcpmarques <miguelcdpmarques@gmail.com>
2019-03-18 10:55:12 +0000

    Fixed sqrt bug

--- best_normalize.py
+++ best_normalize.py
@@ -2,11 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import StandardScaler, PowerTransformer, quantile_transform
-from scipy.stats import normaltest, anderson, boxcox
-from scipy.special import lambertw
-from collections import defaultdict
-import IPython
+from scipy.stats import normaltest, boxcox
 import math
+import IPython
 
 
 class BestNormalize(BaseEstimator, TransformerMixin):
@@ -44,18 +42,13 @@ class BestNormalize(BaseEstimator, TransformerMixin):
         if self.standardize:
             scaler = StandardScaler()
             X = pd.DataFrame(data=scaler.fit_transform(X), columns=X.columns)
-        map_transformation = {'original': lambda x: x, 'log': np.log, 'sqrt': np.sqrt, 
-                        'exp': math.exp, 'arcsin': self.apply_transformation_arcsin, 
-                        'yeo-johnson': self.apply_transformation_yeo_johnson, 
-                        'boxcox': self.apply_transformation_boxcox, 
-                        'quantile':self.apply_transformation_quantile}
-        distribution_methods = ['yeo-johnson', 'boxcox', 'quantile']
+        map_transformation = {'original': lambda x: x, 'log': self.apply_transformation_log,
+                    'sqrt': self.apply_transformation_sqrt, 'exp': self.apply_transformation_exp,
+                    'arcsin': self.apply_transformation_arcsin, 'yeo-johnson': self.apply_transformation_yeo_johnson,
+                    'boxcox': self.apply_transformation_boxcox, 'quantile':self.apply_transformation_quantile}
         for column in X.columns:
             transformation = self.best_scores[self.best_scores['Variable']==column]['Transformation'].iloc[0]
-            if transformation in distribution_methods:
-                X[column] = map_transformation[transformation](X[column])
-            else:
-                X[column] = X[column].apply(map_transformation[transformation])
+            X[column] = map_transformation[transformation](X[column])
         if y:
             transformation = self.best_scores[self.best_scores['Variable']==y.index]['Transformation'].iloc[0]
             y = y.apply(map_transformation[transformation])
@@ -71,7 +64,7 @@ class BestNormalize(BaseEstimator, TransformerMixin):
         alpha = 0.05
         stat, p = normaltest(data)
         if isinstance(stat, np.ndarray):
-            stat, p = np.asscalar(stat), np.asscalar(p)
+            stat, p = stat.item(), p.item()
         test_score = [stat, (p > alpha)]
         return test_score
     
@@ -80,43 +73,64 @@ class BestNormalize(BaseEstimator, TransformerMixin):
         return self.scores.iloc[self.scores.groupby('Variable')['Score'].idxmin()]
     
     def test_transformation_log(self, data):
-        a = min(data) + 1
-        data = np.log(data + a)
-        return ['log'] + self.gaussian_metric(data)
+        a = 1
+        if min(data) <= 0:
+            a = (min(data) * -1) + 1
+        _data = np.log(data + a)
+        return ['log'] + self.gaussian_metric(_data)
     
     def test_transformation_sqrt(self, data):
-        a = min(data) + 1
-        data = np.sqrt(data + a)
-        return ['sqrt'] + self.gaussian_metric(data)
+        a = 1
+        if min(data) <= 0:
+            a = (min(data) * -1) + 1
+        _data = np.sqrt(data + a)
+        return ['sqrt'] + self.gaussian_metric(_data)
     
     def test_transformation_exp(self, data):
         try:
-            data = data.apply(lambda x: math.exp(x))
+            _data = data.apply(lambda x: math.exp(x))
         except OverflowError:
             return ['exp', None, False]
-        return ['exp'] + self.gaussian_metric(data)
+        return ['exp'] + self.gaussian_metric(_data)
 
     def test_transformation_arcsin(self, data):
-        a = min(data) + 1
-        data = np.log([i + np.sqrt(i**2 + 1) for i in data])
-        return ['arcsin'] + self.gaussian_metric(data)
+        _data = np.log([i + np.sqrt(i**2 + 1) for i in data])
+        return ['arcsin'] + self.gaussian_metric(_data)
 
     def test_transformation_yeo_johnson(self, data):
         self._pt = PowerTransformer(method='yeo-johnson', standardize=False, copy=True)
-        data = self._pt.fit_transform(np.array(data).reshape(-1,1))
-        return ['yeo-johnson'] + self.gaussian_metric(data)
+        _data = self._pt.fit_transform(np.array(data).reshape(-1,1))
+        return ['yeo-johnson'] + self.gaussian_metric(_data)
 
     def test_transformation_boxcox(self, data):
         if min(data) < 0:
             return ['boxcox', None, False]
         else:
-            data, _ = boxcox(data)
-            return ['boxcox'] + self.gaussian_metric(data)
+            _data, _ = boxcox(data)
+            return ['boxcox'] + self.gaussian_metric(_data)
 
     def test_transformation_quantile(self, data):
-        data = quantile_transform(np.array(data).reshape(-1,1), output_distribution='normal')
-        return ['quantile'] + self.gaussian_metric(data)
-    
+        _data = quantile_transform(np.array(data).reshape(-1,1), output_distribution='normal')
+        return ['quantile'] + self.gaussian_metric(_data)
+
+    @staticmethod
+    def apply_transformation_log(x):
+        a = 1
+        if min(x) <= 0:
+            a = (min(x) * -1) + 1
+        return np.log(x + a)
+
+    @staticmethod
+    def apply_transformation_sqrt(x):
+        a = 1
+        if min(x) <= 0:
+            a = (min(x) * -1) + 1
+        return np.sqrt(x + a)
+
+    @staticmethod
+    def apply_transformation_exp(x):
+        return x.apply(lambda i: math.exp(i))
+
     @staticmethod
     def apply_transformation_arcsin(x):
         return np.log(x + np.sqrt(x**2 + 1))
@@ -141,5 +155,6 @@ if __name__ == '__main__':
     housing_num = housing.select_dtypes(include=np.number).drop(['latitude','longitude'], axis=1)
 
     bestNormalize = BestNormalize()
-    bestNormalize.fit_transform(housing_num)
+    housing_num_transformed = bestNormalize.fit_transform(housing_num)
     print(bestNormalize.best_scores)
+    print(housing_num_transformed.count())
