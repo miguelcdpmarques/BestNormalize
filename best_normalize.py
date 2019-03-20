094082a4f8d0f1ba930b421199bbb3815dbeb482
miguelcpmarques <miguelcdpmarques@gmail.com>
2019-03-20 15:04:03 +0000

    Started inverse_transform

--- best_normalize.py
+++ best_normalize.py
@@ -15,6 +15,7 @@ class BestNormalize(BaseEstimator, TransformerMixin):
         self.variables = variables
         self.allow_quantile = allow_quantile
         self.scores = []
+        self.transformation_margins = {}
         # if plot: plot both the histogram with the normal fit line and the QQ plots.
         # if verbose: print information about the test statistics
         
@@ -24,11 +25,11 @@ class BestNormalize(BaseEstimator, TransformerMixin):
         for column in X.columns:
             data = X[column].dropna().copy()
             self.scores.extend([[column] + self.test_transformation_log(data)])
-            self.scores.extend([[column] + self.test_transformation_sqrt(data)])
-            self.scores.extend([[column] + self.test_transformation_exp(data)])
-            self.scores.extend([[column] + self.test_transformation_arcsin(data)])
-            self.scores.extend([[column] + self.test_transformation_yeo_johnson(data)])
-            self.scores.extend([[column] + self.test_transformation_boxcox(data)])
+            #self.scores.extend([[column] + self.test_transformation_sqrt(data)])
+            #self.scores.extend([[column] + self.test_transformation_exp(data)])
+            #self.scores.extend([[column] + self.test_transformation_arcsin(data)])
+            #self.scores.extend([[column] + self.test_transformation_yeo_johnson(data)])
+            #self.scores.extend([[column] + self.test_transformation_boxcox(data)])
             if self.allow_quantile:
                 self.scores.extend([[column] + self.test_transformation_quantile(data)])
         self.scores = pd.DataFrame(data=self.scores, 
@@ -36,7 +37,7 @@ class BestNormalize(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X, y=None):
-        map_transformation = {'original': lambda x: x, 'log': self.apply_transformation_log,
+        map_transformation = {'original': self.apply_no_transformation, 'log': self.apply_transformation_log,
                     'sqrt': self.apply_transformation_sqrt, 'exp': self.apply_transformation_exp,
                     'arcsin': self.apply_transformation_arcsin, 
                     'yeo-johnson': self.apply_transformation_yeo_johnson,
@@ -50,7 +51,14 @@ class BestNormalize(BaseEstimator, TransformerMixin):
         return X
     
     def inverse_transform(self, X):
-        pass
+        if self.standardize:
+            X = pd.DataFrame(data=self.scaler.inverse_transform(X), columns=X.columns)
+        map_inverse_transformation = {'original': self.apply_no_transformation, 
+                    'log': self.apply_inverse_transformation_log}
+        for column in X.columns:
+            transformation = self.best_scores[self.best_scores['Variable']==column]['Transformation'].iloc[0]
+            X[column] = map_inverse_transformation[transformation](X[column])
+        return X
 
     def init_scores(self, X):
         for column in X.columns:
@@ -111,12 +119,18 @@ class BestNormalize(BaseEstimator, TransformerMixin):
         return ['quantile'] + self.gaussian_metric(_data)
 
     @staticmethod
-    def apply_transformation_log(x):
-        a = 1
-        if min(x) <= 0:
-            a = (min(x) * -1) + 1
-        return np.log(x + a)
+    def apply_no_transformation(x):
+       return x
 
+    def apply_transformation_log(self, x):
+        self.transformation_margins[x.name] = 1
+        if min(x) <= 0:
+            self.transformation_margins[x.name] = (min(x) * -1) + 1
+        return np.log(x + self.transformation_margins[x.name])
+    
+    def apply_inverse_transformation_log(self, x):
+        return np.exp(x - self.transformation_margins[x.name])
+        
     @staticmethod
     def apply_transformation_sqrt(x):
         a = 1
@@ -154,7 +168,10 @@ if __name__ == '__main__':
     housing = pd.read_csv('datasets/housing.csv')
     housing_num = housing.select_dtypes(include=np.number).drop(['latitude','longitude'], axis=1)
 
-    bestNormalize = BestNormalize()
+    bestNormalize = BestNormalize(allow_quantile=False)
     housing_num_transformed = bestNormalize.fit_transform(housing_num)
+    housing_num_inverse = bestNormalize.inverse_transform(housing_num_transformed)
     print(bestNormalize.best_scores)
-    print(housing_num_transformed.count())
+    print(np.allclose(housing_num, housing_num_inverse))
+    print(housing_num[:5])
+    print(housing_num_inverse[:5])
